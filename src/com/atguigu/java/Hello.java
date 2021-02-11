3e5c40ce3d0eccfcaf83c9f7a3ec5d508f3fbcb9
Anzl <anzhaoliang1@163.com>
2021-02-11 14:14:07 +0800

    Initial commit

---
+++ src/com/atguigu/java/Hello.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author 安兆亮
+ * @date 2021/2/8 - 21:29
+ */
+public class Hello {
+}
---
+++ src/com/atguigu/java/TemplatesTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.java;
+
+import java.util.Arrays;
+
+/**
+ * @author 安兆亮
+ * @date 2021/2/10 - 15:43
+ */
+public class TemplatesTest {
+    public static final int m = 4;
+
+    public static void main(String[] args) {
+
+    }
+    public void testupdata(){
+
+    }
+
+
+}
